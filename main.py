cf9d510896efdf58c13db359d2675c20a699b460
ahffank <ahffank@gmail.com>
2020-06-29 11:51:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ from flask_cors import CORS,cross_origin
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+@app.route("/")
+def index():
+	return render_template("index.html")
 #!pwd
 
 #from google.colab import drive
